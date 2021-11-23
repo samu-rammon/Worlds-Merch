c49ff678f51c6d839ccfd25c60f46d14e9bd55ce
samuloko <samuel.silva.estudante@eteot.rio.br>
2021-11-22 21:00:52 -0300

    i

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let rolamento
+
+function rolar() {
+    if (scrollY > rolamento) {
+        document.getElementById("nav").style.marginTop='20px';
+    } else () {
+        document.getElementById("nav").style.marginTop='00px';
+    }
+}
